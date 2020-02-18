173b0df280dc13b517a2322eb14482a462efa0c4
Nethelbert <gilblasse@gmail.com>
2020-02-18 04:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 const upperCaseCurrentUser = currentUser.toUpperCase()
-const welcomeMessage = `Welcome to Flatbook, ${upperCaseCurrentUser}`
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`
 const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()} !`
